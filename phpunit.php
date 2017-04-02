99d03ae5a652375ad62ed8f93d04c29e7f6593a4
Roy de Jong <roy@softwarepunt.nl>
2017-04-02 15:22:07 +0200

    PhpUnit bootstrapper: Reset to default config

--- phpunit.php
+++ phpunit.php
@@ -12,16 +12,16 @@ define('TEST_USER_NAME', 'root');
 define('TEST_PASSWORD', 'dev123');
 
 // Reset database
-//$pdo = new \PDO('mysql:host=localhost;dbname=' . TEST_DATABASE_NAME . ';charset=utf8', TEST_USER_NAME, TEST_PASSWORD);
-//
-//$pdo->exec('DROP TABLE IF EXISTS users;');
-//
-//$pdo->exec('CREATE TABLE `testdb`.`users` (
-//  `id` INT NOT NULL AUTO_INCREMENT,
-//  `user_name` VARCHAR(45) NULL,
-//  `email_address` VARCHAR(45) NULL,
-//  `join_date` DATETIME NULL,
-//  PRIMARY KEY (`id`),
-//  UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC));');
+$pdo = new \PDO('mysql:host=localhost;dbname=' . TEST_DATABASE_NAME . ';charset=utf8', TEST_USER_NAME, TEST_PASSWORD);
+
+$pdo->exec('DROP TABLE IF EXISTS users;');
+
+$pdo->exec('CREATE TABLE `testdb`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NULL,
+  `email_address` VARCHAR(45) NULL,
+  `join_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC));');
 
 $pdo = null;
