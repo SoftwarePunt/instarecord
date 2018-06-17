1ab0898c14ba396d7915b9b8228bfee45c7be226
Roy de Jong <roy@softwarepunt.nl>
2018-06-17 06:54:41 +0200

    CI/Tests: Fix TestDatabaseConfig helper class / related tests I hope

--- tests/Testing/TestDatabaseConfig.php
+++ tests/Testing/TestDatabaseConfig.php
@@ -9,9 +9,11 @@ class TestDatabaseConfig extends DatabaseConfig
 {
     public function __construct()
     {
-        $this->adapter = DatabaseAdapter::MYSQL;
+        $this->host = TEST_DATABASE_HOST;
+        $this->port = TEST_DATABASE_PORT;
         $this->username = TEST_USER_NAME;
         $this->password = TEST_PASSWORD;
         $this->database = TEST_DATABASE_NAME;
+        $this->charset = "utf8mb4";
     }
 }
