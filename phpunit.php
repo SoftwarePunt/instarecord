2da8a81a009cdfd70d2647de35b2d858134566c9
Roy de Jong <roy@softwarepunt.nl>
2018-06-17 04:27:21 +0000

    Update phpunit.php

--- phpunit.php
+++ phpunit.php
@@ -7,16 +7,18 @@
 // Initialize the class autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
-// PHPUnit config
+// DB Config: Load from JSON, generate DSN, move on :-)
 $config = json_decode(file_get_contents(__DIR__ . '/phpunit-config.json'), true);
 
-// Reset database
-$pdo = new \PDO($config['db_connection'], $config['db_user'], $config['db_pass']);
-
 // Constants for backwards compat
+define('TEST_DATABASE_HOST', $config['db_host']);
+define('TEST_DATABASE_PORT', $config['db_port']);
 define('TEST_USER_NAME', $config['db_user']);
 define('TEST_PASSWORD', $config['db_pass']);
 define('TEST_DATABASE_NAME', $config['db_name']);
+
+// Local PDO instance to seed db
+$pdo = new \PDO($config['db_connection'], $config['db_user'], $config['db_pass']);
 
 $pdo->exec('DROP TABLE IF EXISTS users;');
 
