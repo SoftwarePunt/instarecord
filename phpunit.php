d9b06de93422940440615f282f62cf1a853539ca
Roy de Jong <roy@softwarepunt.nl>
2018-06-17 03:47:18 +0000

    Update phpunit.php

--- phpunit.php
+++ phpunit.php
@@ -16,6 +16,7 @@ $pdo = new \PDO($config['db_connection'], $config['db_user'], $config['db_pass']
 // Constants for backwards compat
 define('TEST_USER_NAME', $config['db_user']);
 define('TEST_PASSWORD', $config['db_pass']);
+define('TEST_DATABASE_NAME', $config['db_name']);
 
 $pdo->exec('DROP TABLE IF EXISTS users;');
 
